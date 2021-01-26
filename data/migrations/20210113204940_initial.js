1c91dd23105dc78c5608168563c370dc53eaf5d3
rdowd <rjarrettdowd@yahoo.com>
2021-01-25 21:04:44 -0500

    Added cat and service tables for services page.

--- data/migrations/20210113204940_initial.js
+++ data/migrations/20210113204940_initial.js
@@ -1,24 +1,42 @@
 
 exports.up = async (knex) => {
     await knex.schema.createTable("users", (table => {
-      table.increments("id")
-      table.string("username").notNull().unique()
-      table.string("password").notNull()
+        table.increments("id")
+        table.string("username").notNull().unique()
+        table.string("password").notNull()
     }))
 
     await knex.schema.createTable("message", (table => {
+        table.increments("id")
         table.boolean("status")
         table.string("title")
         table.string("message", 10000)
     }))
 
     await knex.schema.createTable("videos", (table => {
+        table.increments("id")
         table.string("name")
         table.string("url")
     }))
+
+    await knex.schema.createTable("serv_cat", (table => {
+        table.increments("id")
+        table.string("category")
+    }))
+
+    await knex.schema.createTable("serv_service", (table => {
+        table.increments("id")
+        table.string("service")
+        table.integer("cat_id").references("id").inTable("serv_cat")
+    }))
+
+    
 };
 
 exports.down = async (knex) => {
+    await knex.schema.dropTableIfExists("serv_service")
+    await knex.schema.dropTableIfExists("serv_cat")
+    await knex.schema.dropTableIfExists("videos")
     await knex.schema.dropTableIfExists("message")
     await knex.schema.dropTableIfExists("users")
 };
